06639d55bb36f3fd268cfb53ff58c7d86abc1791
Liam Bowers <liamondrop@gmail.com>
2018-12-04 08:58:16 -0800

    decoupling board controller

---
+++ include/pca9685_board/pca9685_board.h
@@ -0,0 +1,21 @@
+#ifndef PCA9685Board_H_
+#define PCA9685Board_H_
+
+class PCA9685Board
+{
+public:
+    PCA9685Board();
+    int setup(const int i2c_address, float pwm_freq);
+    void set_pwm_interval(int pin, int value);
+
+private:
+    void reset_all_();
+    void set_pwm_freq_(float pwm_freq);
+    void pwm_write_(int pin, int on, int off);
+    void full_on_(int pin, int tf);
+    void full_off_(int pin, int tf);
+
+    int io_handle_;
+};
+
+#endif  // PCA9685Board_H_
--- src/pca9685_board.cpp
+++ src/pca9685_board.cpp
@@ -1,10 +1,5 @@
-#include <ros/ros.h>
-#include <geometry_msgs/Twist.h>
-
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
-
-#include "pca9685_board/Servo.h"
 
 // Setup registers
 #define PCA9685_MODE1    0x0
@@ -18,9 +13,6 @@
 #define PIN_BASE    300
 #define MAX_PWM     4096
 
-#define I2C_ADDRESS 0x40
-#define HERTZ       50
-
 
 /**
  * Helper function to get to register
@@ -30,133 +22,9 @@ int base_register_(const int pin)
     return (pin >= PIN_ALL ? LEDALL_ON_L : LED0_ON_L + 4 * pin);
 }
 
-int get_int_param_(XmlRpc::XmlRpcValue obj, std::string param_name)
-{
-    XmlRpc::XmlRpcValue &item = obj[param_name];
-    if (item.getType() == XmlRpc::XmlRpcValue::TypeInt)
-        return item;
-
-    ROS_WARN("invalid paramter type for %s - expected TypeInt", param_name.c_str());
-    return 0;
-}
-
-
-class PCA9685Board
-{
-public:
-    PCA9685Board();
-    int setup(const int i2c_address, float pwm_freq);
-    void set_pwm_interval(int pin, int value);
-
-private:
-    void get_servo_params_(XmlRpc::XmlRpcValue servos);
-    void servo_absolute_(const pca9685_board::Servo::ConstPtr& msg);
-    void reset_all_();
-    void set_pwm_freq_(float pwm_freq);
-    void pwm_write_(int pin, int on, int off);
-    void full_on_(int pin, int tf);
-    void full_off_(int pin, int tf);
-
-    int io_handle_;
-
-    ros::NodeHandle nh_;
-    ros::Subscriber abs_sub_;
-};
-
-
 PCA9685Board::PCA9685Board()
 {
-    if (0 > setup(I2C_ADDRESS, HERTZ))
-    {
-        ROS_ERROR("Error in setup.");
-        return;
-    }
-
-    abs_sub_ = nh_.subscribe<pca9685_board::Servo>("servo_absolute", 1, &PCA9685Board::servo_absolute_, this);
-}
-
-void PCA9685Board::get_servo_params_(XmlRpc::XmlRpcValue servos)
-{
-    // if (nh_.hasParam("servos"))
-    // {
-    //     XmlRpc::XmlRpcValue servos_;
-    //     nh_.getParam("servos", servos_);
-
-    //     if (servos.getType() == XmlRpc::XmlRpcValue::TypeArray)
-    //     {
-    //         ROS_INFO(
-    //             "Retrieving members from 'servos_' in namespace(%s)",
-    //             nh_.getNamespace().c_str());
-                
-    //         for (int i = 0; i < servos.size(); ++i)
-    //         {
-    //             XmlRpc::XmlRpcValue servo;
-    //             servo = servos[i];	// get the data from the iterator
-    //             if (servo.getType() == XmlRpc::XmlRpcValue::TypeStruct)
-    //             {
-    //                 ROS_INFO(
-    //                     "Retrieving items from 'servo_config' member %d in namespace(%s)",
-    //                     i, nh_.getNamespace().c_str());
-
-    //                 // get the servo settings
-    //                 int id, center, direction, range;
-    //                 int id = get_int_param_(servo, "servo");
-    //                 int center = get_int_param_(servo, "center");
-    //                 int direction = get_int_param_(servo, "direction");
-    //                 int range = get_int_param_(servo, "range");
-                    
-    //                 if (id && center && direction && range)
-    //                 {
-    //                     if ((id >= 1) && (id <= MAX_SERVOS))
-    //                     {
-    //                         int board = ((int)(id / 16)) + 1;
-    //                         _set_active_board (board);
-    //                         _set_pwm_frequency (pwm);
-    //                         _config_servo (id, center, range, direction);
-    //                     }
-    //                     else
-    //                     {
-    //                         ROS_WARN("Parameter servo=%d is out of bounds", id);
-    //                     }
-    //                 }
-    //                 else
-    //                 {
-    //                     ROS_WARN("Invalid parameters for servo=%d'", id);
-    //                 }
-    //             }
-    //             else
-    //             {
-    //                 ROS_WARN(
-    //                     "Invalid type %d for member of 'servo_config' - expected TypeStruct(%d)",
-    //                     servo.getType(), XmlRpc::XmlRpcValue::TypeStruct);
-    //             }
-    //         }
-    //     }
-    //     else {
-    //         ROS_WARN(
-    //             "Invalid type %d for 'servos' - expected TypeArray(%d)",
-    //             servos_.getType(), XmlRpc::XmlRpcValue::TypeArray);
-    //     }
-    // }
-}
-
-/**
- * Subscriber for the servo_absolute topic which processes a servo and sets its physical pulse value.
- *
- * the following messages are an example of finding a continuous servo's center
- * in this example the center is found to be 333
- * 
- * rostopic pub servo_absolute pca9685_board/Servo "{servo: 1, value: 300}"
- * rostopic pub servo_absolute pca9685_board/Servo "{servo: 1, value: 350}"
- * rostopic pub servo_absolute pca9685_board/Servo "{servo: 1, value: 320}"
- * rostopic pub servo_absolute pca9685_board/Servo "{servo: 1, value: 330}"
- * rostopic pub servo_absolute pca9685_board/Servo "{servo: 1, value: 335}"
- * rostopic pub servo_absolute pca9685_board/Servo "{servo: 1, value: 333}"
- * rostopic pub -1 /servos_absolute i2cpwm_board/ServoArray "{servos:[{servo: 1, value: 300}]}"
- */
-void PCA9685Board::servo_absolute_(const pca9685_board::Servo::ConstPtr& msg)
-{
-    PCA9685Board::set_pwm_interval(msg->servo, msg->value);
+    wiringPiSetupGpio();
 }
 
 /**
@@ -168,8 +36,6 @@ void PCA9685Board::servo_absolute_(const pca9685_board::Servo::ConstPtr& msg)
  */
 int PCA9685Board::setup(const int i2c_address, float pwm_freq)
 {
-    wiringPiSetupGpio();
-
     // Create a node with 16 pins [0..15] + [16] for all
     struct wiringPiNodeStruct *node = wiringPiNewNode(PIN_BASE, PIN_ALL + 1);
 
@@ -306,14 +172,4 @@ void PCA9685Board::full_off_(int pin, int tf)
     state = tf ? (state | 0x10) : (state & 0xEF);
 
     wiringPiI2CWriteReg8(io_handle_, reg, state);
-}
-
-int main(int argc, char** argv)
-{
-    ros::init(argc, argv, "pca9685_board_node");
-    PCA9685Board board;
-
-    ros::spin();
-
-    return 0;
 }
